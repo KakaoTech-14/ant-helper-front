75520e17b30eb9d7cf150e231bd002cbf5698364
extinctmule <extinctmule@gmail.com>
2024-08-18 20:59:40 +0900

    Feat: add navigation bar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,13 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 
-import { Form, Input, Title, Wrapper } from "../components/Common";
+import Navbar from "../components/Navbar";
+import { Wrapper } from "../components/Common";
 
 const Home = () => {
   return (
     <Wrapper>
-      <Title>Ant Helper</Title>
-      <Link to="/signin">로그인</Link>
-      <Link to="/signup">회원가입</Link>
+      <Navbar />
+      Home
     </Wrapper>
   );
 };
