dd9d855e1ef8063f32864c6eb99a08871ccdd89a
extinctmule <extinctmule@gmail.com>
2024-08-19 21:58:03 +0900

    Update codes

---
+++ src/pages/Mypage.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+import Navbar from "../components/Navbar";
+import { Wrapper } from "../components/Common";
+
+const Mypage = () => {
+  return (
+    <Wrapper>
+      <Navbar />
+      Mypage
+    </Wrapper>
+  );
+};
+
+export default Mypage;
