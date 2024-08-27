7097a209621d4d8afbd44dcb4556df9dd836946a
extinctmule <extinctmule@gmail.com>
2024-08-27 21:35:40 +0900

    Feat: 계좌 페이지 추가

---
+++ src/pages/Account.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+import Navbar from "../components/Navbar";
+import { Wrapper } from "../components/Common";
+
+const Account = () => {
+  return (
+    <Wrapper>
+      <Navbar />내 계좌
+    </Wrapper>
+  );
+};
+
+export default Account;
