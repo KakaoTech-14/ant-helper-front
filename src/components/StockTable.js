3d80485e617d487b14fc3702654f3b793084fb96
extinctmule <extinctmule@gmail.com>
2024-09-09 23:29:07 +0900

    Design: min width 추가

--- src/components/StockTable.js
+++ src/components/StockTable.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const StockTableContainer = styled.div`
   width: 60%;
   max-width: 600px;
+  min-width: 300px;
   padding-left: 20px;
   box-sizing: border-box;
 `;
@@ -10,6 +11,7 @@ export const StockTableContainer = styled.div`
 export const Table = styled.table`
   width: 100%;
   max-width: 600px;
+  min-width: 300px;
   border-collapse: collapse;
 `;
 
