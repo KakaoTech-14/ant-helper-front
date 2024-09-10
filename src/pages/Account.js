4ec42b0c2f257460cd2491b59ccde92e9da7b640
extinctmule <extinctmule@gmail.com>
2024-09-10 15:31:47 +0900

    Feat: 내 주식 현황 기능 추가

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,15 +1,23 @@
 import React from 'react';
-
+import { NavLink, Route, Routes } from 'react-router-dom';
 import NavBar from '../components/NavBar';
-import { Body, Wrapper } from '../components/Common';
+import { Wrapper } from '../components/Common';
 import Sidebar from '../components/Sidebar/Sidebar';
+import AccountOverview from '../components/Account/AccountOverview'; // 내 주식 현황 컴포넌트
+import Transactions from '../components/Account/Transactions'; // 체결 내역 컴포넌트
 
 const Account = () => {
   return (
     <Wrapper>
       <NavBar />
-      <div className={'w-full'}>
+      <div className="w-full flex">
         <Sidebar />
+        <div className="flex-1">
+          <Routes>
+            <Route path="/" element={<AccountOverview />} />
+            <Route path="/transactions" element={<Transactions />} />
+          </Routes>
+        </div>
       </div>
     </Wrapper>
   );
