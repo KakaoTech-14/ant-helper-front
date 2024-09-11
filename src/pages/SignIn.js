ecba1c827bfd8a802ecf9387151237cdd9503ad8
extinctmule <extinctmule@gmail.com>
2024-09-11 11:37:39 +0900

    Style: 코드 순서 변경

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -5,6 +5,15 @@ import { styled } from 'styled-components';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 
+const CustomLink = styled(Link)`
+  margin: 10px;
+  color: black;
+  &:visited {
+    color: grey;
+    text-decoration: none;
+  }
+`;
+
 const SignIn = () => {
   const [email, setEmail] = useState('');
   const [pw, setPW] = useState('');
@@ -80,12 +89,3 @@ const SignIn = () => {
 };
 
 export default SignIn;
-
-const CustomLink = styled(Link)`
-  margin: 10px;
-  color: black;
-  &:visited {
-    color: grey;
-    text-decoration: none;
-  }
-`;
